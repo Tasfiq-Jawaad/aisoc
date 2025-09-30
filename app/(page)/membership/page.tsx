c0ecb72aa13c5d2a0d3730f3df2856c164e321af
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2025-09-30 07:48:17 +0100

    feat: create a placeholder membership page and redirect to tengage

---
+++ app/(page)/membership/page.tsx
@@ -0,0 +1,8 @@
+import { redirect } from "next/navigation";
+
+export default function ContactPage() {
+  redirect(
+    "https://engage.luu.org.uk/groups/4GQD2/artificial-intelligence-society/memberships"
+  );
+  return <div></div>;
+}
