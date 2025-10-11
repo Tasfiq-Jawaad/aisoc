4760b3e27b917d549aaa4cb4c97ee5c1b04faf44
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2025-10-11 09:11:45 +0100

    feat(check-in): turn register buttons to check in buttons

--- app/hack_ai_thon/page.tsx
+++ app/hack_ai_thon/page.tsx
@@ -44,7 +44,7 @@ export default function HackathonPage() {
 
           <div className="mt-8 flex flex-wrap gap-3">
             <Link href="/hack_ai_thon/register" className="hack-btn-primary">
-              Register now
+              Check in
             </Link>
             <a href="#theme" className="hack-btn-ghost">
               Learn more
@@ -303,8 +303,8 @@ export default function HackathonPage() {
               </p>
             </div>
             <div className="flex gap-3">
-              <Link href="/hack_ai_thon/register" className="hack-btn-primary">
-                Grab your spot
+              <Link href="/hack_ai_thon/check-in" className="hack-btn-primary">
+                Check in
               </Link>
             </div>
           </div>
