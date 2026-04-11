8ce16d0194301b12618d24d71b07d1e1bf1434ef
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2026-04-11 12:55:59 +0100

    feat(events): add extended urls and slug to event type

--- types/event.ts
+++ types/event.ts
@@ -3,12 +3,16 @@ import { UUID } from "./database";
 export interface Event {
   id: UUID; // string
   created_at: string; // timestamptz
-  title: string | null;
+  slug: string; // for url
+  title: string;
   start_date: string | null; // timestamp (without tz)
   end_date: string | null; // timestamp (without tz)
-  is_active: boolean | null;
+  is_active: boolean | true;
   location: string | null;
+  map_url: string | null;
   badge: string | null;
   image_url: string | null;
   engage_url: string | null;
+  ticket_url: string | null;
+  note_url: string | null;
 }
