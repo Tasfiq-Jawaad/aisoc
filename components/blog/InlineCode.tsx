3afee31de89f865f468607da321a4d52be2611f3
Tasfiq-Jawaad <tasfiqjawaad@gmail.com>
2026-04-11 03:23:45 +0100

    chore(blog): use global style for inline code

--- components/blog/InlineCode.tsx
+++
@@ -1,5 +0,0 @@
-export const InlineCode = ({ children }: { children: React.ReactNode }) => (
-  <code className="rounded bg-white/10 px-1 py-0.5 font-mono text-[0.95em] text-gray-100">
-    {children}
-  </code>
-);
